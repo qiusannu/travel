4abddaa17a8dd5b14e2fb1d66948ee31ce7772a9
honeyqkl <32706176+honeyqkl@users.noreply.github.com>
2020-02-21 20:16:47 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<!--单文件组件 vue的组件-->
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+}
+</style>
