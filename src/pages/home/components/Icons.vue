b3a4c59b8a203ce36d48a377d7486bc579b8c501
honeyqkl <32706176+honeyqkl@users.noreply.github.com>
2020-02-23 20:37:21 +0800

    add icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="icons">
+    <swiper>
+      <swiper-slide v-for="(page , index) of pages" :key="index">
+        <div class="icon" v-for="item of page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl" alt="">
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeIcons',
+  data: function () {
+    return {
+      iconList: [{
+        id: '1001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票'
+      }, {
+        id: '1002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/fc/b10a6b2e4f0fe102.png',
+        desc: '万龙滑雪'
+      }, {
+        id: '1003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/6c/9e54a8540fee0102.png',
+        desc: '故宫'
+      }, {
+        id: '1004',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/fc/b10a6b2e4f0fe102.png',
+        desc: '静之湖滑雪'
+      }, {
+        id: '1005',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+        desc: '一日游'
+      }, {
+        id: '1006',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/67/9a1678221b8e0e02.png',
+        desc: '古北水镇'
+      }, {
+        id: '1007',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/fc/b10a6b2e4f0fe102.png',
+        desc: '北京滑雪'
+      }, {
+        id: '1008',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/fc/b10a6b2e4f0fe102.png',
+        desc: '陶然亭冰雪'
+      }, {
+        id: '1009',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/ab/6f7d6e44963c9302.png',
+        desc: '泡温泉'
+      }, {
+        id: '1010',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/47/c2b659e048b11602.png',
+        desc: '中国马镇'
+      }]
+    }
+  },
+  computed: {
+    pages: function () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        // item在第几页记录在了page中 生成一个二维数组
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  @import "~styles/varibles.styl"
+  @import "~styles/mixins.styl"
+  .icons >>> .swiper-container
+    height: 0
+    padding-bottom :50%
+  .icon
+    position: relative
+    overflow: hidden
+    float: left
+    height: 0
+    width: 25%
+    padding-bottom: 25%
+
+    .icon-img
+      position: absolute
+      top: 0
+      left: 0
+      right: 0
+      box-sizing: border-box
+      bottom: 0.44rem
+      padding: .1rem
+
+      .icon-img-content
+        height: 100%
+        margin: 0 auto
+        display: block
+
+    .icon-desc
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      text-align: center
+      height: .44rem
+      line-height: .44rem
+      color: $darkTextColor
+      ellipsis()
+</style>
